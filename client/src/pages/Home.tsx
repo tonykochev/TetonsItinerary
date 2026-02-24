fa7e99667eb5347801f4dd6a4cd5d8682137427d
“tonykochev” <tonykochev@gmail.com>
2026-02-23 22:33:11 -0600

    some changes to paragraphs

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -29,13 +29,15 @@ const Home: React.FC = () => {
           className="feature-card clickable"
           onClick={() => navigate("/hikes")}
           >
-          <h3>🥾 Trails & Activities</h3>
+          <h3>🥾 Trails</h3>
           <p>Explore hikes, lakes, wildlife, and scenic routes.</p>
         </div>
 
-        <div className="feature-card">
-          <h3>📅 Save & Edit</h3>
-          <p>Update your plan anytime and access it anywhere.</p>
+        <div className="feature-card clickable"
+        onClick={() => navigate("/camping")}
+        >
+          <h3>🏕️ Camping</h3>
+          <p>Explore the Tetons camping grounds.</p>
         </div>
       </section>
 
@@ -47,10 +49,17 @@ const Home: React.FC = () => {
         <div className="about-text">
             <h2>About Me</h2>
                 <p>
-                Hi! I’m Tony, a computer science and data science student who loves building
-                useful apps and exploring national parks. I created the Tetons Itinerary
-                Builder to help people plan unforgettable trips without the stress of
-                figuring everything out themselves.
+                Hey! I'm Tony. I created this site to share the kind of trip planning I’ve learned 
+                through years of exploring national parks firsthand. I’ve spent a full week camping 
+                and hiking in Grand Teton National Park after driving out from the Midwest, and I’m 
+                returning this summer—this website is part of how I’m planning that trip. My experience 
+                includes backpacking and camping in Zion National Park, hiking Angels Landing, and camping 
+                and exploring trails throughout Yosemite National Park. I’ve also hiked sections of the 
+                Appalachian Trail in Great Smoky Mountains National Park, climbed three 14ers in Colorado, 
+                and spent time backcountry and public land camping in the Rockies. My love for the outdoors 
+                started as a kid on family trips to places like the Grand Canyon, and it’s grown into 
+                a passion for planning smart, realistic adventures. This site reflects real experience on 
+                the trail and is designed to help others plan a trip to the Tetons with confidence.
                 </p>
             </div>
         </section>

82eeb7c0ce7a54e664993564a5f2745a7bb0358a
“tonykochev” <tonykochev@gmail.com>
2026-02-26 12:52:04 -0600

    cahnged the urls to include the https

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -1,4 +1,4 @@
 // api url (where your server is hosted at)
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'tetonitinerarybackend-dheedzcfbvhpetfm.canadacentral-01.azurewebsites.net'
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'https://tetonitinerarybackend-dheedzcfbvhpetfm.canadacentral-01.azurewebsites.net'
 
 export { BACKEND_URL }
